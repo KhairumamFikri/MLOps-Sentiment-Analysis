68f9cd620dad0b93091264a2938919d89fcc8db8
KhairumamFikri <khairumamfikri2004@gmail.com>
2026-06-10 07:02:32 +0700

    feat: dashboard grafana dan prometheus

--- app/main.py
+++ app/main.py
@@ -1,4 +1,14 @@
 from fastapi import FastAPI
+from fastapi.responses import Response
+
+from prometheus_client import (
+    Counter,
+    Histogram,
+    generate_latest,
+    CONTENT_TYPE_LATEST
+)
+
+from time import time
 
 from app.schemas import (
     PredictionRequest,
@@ -13,6 +23,36 @@ app = FastAPI(
     title="Sentiment Analysis API"
 )
 
+# =====================================
+# Metrics
+# =====================================
+
+REQUEST_COUNT = Counter(
+    "request_count_total",
+    "Total prediction requests"
+)
+
+REQUEST_LATENCY = Histogram(
+    "request_latency_seconds",
+    "Prediction latency"
+)
+
+POSITIVE_COUNT = Counter(
+    "prediction_positive_total",
+    "Positive predictions"
+)
+
+NEGATIVE_COUNT = Counter(
+    "prediction_negative_total",
+    "Negative predictions"
+)
+
+NEUTRAL_COUNT = Counter(
+    "prediction_neutral_total",
+    "Neutral predictions"
+)
+
+# =====================================
 
 @app.get("/")
 def root():
@@ -20,6 +60,15 @@ def root():
     return {
         "message": "API running"
     }
+
+
+@app.get("/metrics")
+def metrics():
+
+    return Response(
+        generate_latest(),
+        media_type=CONTENT_TYPE_LATEST
+    )
 
 
 @app.post(
@@ -30,9 +79,28 @@ def predict(
     request: PredictionRequest
 ):
 
+    REQUEST_COUNT.inc()
+
+    start = time()
+
     sentiment = predict_sentiment(
         request.text
     )
+
+    latency = time() - start
+
+    REQUEST_LATENCY.observe(
+        latency
+    )
+
+    if sentiment == "positive":
+        POSITIVE_COUNT.inc()
+
+    elif sentiment == "negative":
+        NEGATIVE_COUNT.inc()
+
+    else:
+        NEUTRAL_COUNT.inc()
 
     return {
         "sentiment": sentiment
